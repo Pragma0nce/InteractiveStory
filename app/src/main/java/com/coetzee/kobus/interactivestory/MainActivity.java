70be364ffd8a7d581843a78912ffaa289ca3d091
Pragma0nce <kobus.coetzeee@gmail.com>
2016-10-20 21:23:10 +0200

    Added the Story Activity

--- app/src/main/java/com/coetzee/kobus/interactivestory/MainActivity.java
+++ app/src/main/java/com/coetzee/kobus/interactivestory/MainActivity.java
@@ -1,13 +1,40 @@
 package com.coetzee.kobus.interactivestory;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mNameField;
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNameField = (EditText)findViewById(R.id.nameEditText);
+        mButton = (Button)findViewById(R.id.startButton);
+
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = mNameField.getText().toString();
+                startStory();
+            }
+        });
+
+    }
+
+    private void startStory()
+    {
+        Intent intent = new Intent(this,StoryActivity.class);
+        startActivity(intent);
     }
 }
